2c742aa18e084abb40277c8594c9c23698e28979
Ivan A Kireev <ivkireev@yandex.ru>
2022-05-18 12:22:42 +0300

    clean repo

--- vtb_code/utils.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-def logging_init(handlers=None):
-    logging.basicConfig(level=logging.INFO, format='%(funcName)-20s   : %(message)s',
-                        handlers=handlers)
